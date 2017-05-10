a23a398e4f493ea97b7045fecd7ef86e57d1cdf4
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2017-05-11 00:22:09 +0200

    Another small change. (created benchmarks.h)
    TODO: squash thi in the next commit too (with the previous one)

--- mpi_matmul/matmul-mpi.h
+++ mpi_matmul/matmul-mpi.h
@@ -2,43 +2,7 @@
 #ifndef matmul_h
 #define  matmul_h
 
-// C headers
-#include <mpi.h>
-#include <omp.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <stdbool.h>
-#include <string.h>
-
-#define imin(x, y) (((x) < (y)) ? (x) : (y))
-#define imax(x, y) (((x) > (y)) ? (x) : (y))
-
-#define frand()(4.*(double)rand()/(RAND_MAX)-2.) //uniform rng in [-2,2]
-
-#define printme() {                                             \
-        fprintf(stderr,"Func: %s in %s:%d (process %s)\n",      \
-                __PRETTY_FUNCTION__, __FILE__, __LINE__,        \
-                getenv("OMPI_COMM_WORLD_RANK"));                \
-        }
-
-#define modcheck(a, b){                                         \
-        const int themod=(a) % (b);                             \
-        if(themod) {                                            \
-            fprintf(stderr,"Error: %s %% %s = %d\n", #a, #b, themod);   \
-            printme();                                          \
-            MPI_Abort(MPI_COMM_WORLD, -1);                      \
-            }                                                   \
-        }
-
-#define myassert(x){                                            \
-        if(!(x)){                                               \
-            fprintf(stderr,"Error: %s returned %d\n",#x, x);    \
-            printme();                                          \
-            MPI_Abort(MPI_COMM_WORLD, -1);                      \
-            }                                                   \
-        }
-
+#include "benchmarks.h"
 
 #ifdef __cplusplus
 extern "C" {

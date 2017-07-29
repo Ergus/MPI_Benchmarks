ae793bab535512a7c28e795d61c534cd558569b0
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2017-07-29 17:35:45 +0200

    Improved the submit scripts test[1,2].sh

--- mpi_dynamic_process/test2.sh
+++ mpi_dynamic_process/test2.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=mpi_dynamic_delete
 #SBATCH --qos=debug
-#SBATCH --time=00-00:05:00
+#SBATCH --time=00-00:10:00
+#SBATCH --nodes=16
 #SBATCH --error=%x_%A.err 
 #SBATCH --output=%x_%A.out
-#SBATCH --nodes=16
 #SBATCH --tasks-per-node=1
 
+if (($# < 1)); then
+    (>&2 echo "Error missing parameters")
+    exit
+fi
+
 for i in 1 2 4 8 16; do
+    END=$1
     times=$((SLURM_JOB_NUM_NODES/i))     # Estimates nproc/step
     char='%.0s -d '$i                    # generates a substring '%0s -s step'
     input=$(printf "${char}" $(eval echo {1..$times}))
     echo "Running times="$times
     echo $input
-    mpirun -np 1 ./dynamicOOP -s 16 ${input} -i | tee ${SLURM_JOB_NAME}_${SLURM_JOB_ID}_${SLURM_JOB_NUM_NODES}_$i.out
+    for ((a=1; a<=END; ++a)); do
+	mpirun -np 1 ./dynamicOOP -s 16 -i ${input} | tee ${SLURM_JOB_NAME}_${SLURM_JOB_ID}_${SLURM_JOB_NUM_NODES}_$i.out
+    done
 done

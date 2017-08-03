7f362cb67e72206b44ccde466e1c58f4dc8f44c2
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2017-08-03 22:48:50 +0200

    Improved test3.sh in mpi_dynamic test.

--- mpi_dynamic_process/test3.sh
+++ mpi_dynamic_process/test3.sh
@@ -1,27 +1,41 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=mpi_dynamic_spawn
-#SBATCH --qos=debug
-#SBATCH --time=00-00:10:00
+#SBATCH --qos=bsc_cs
+#SBATCH --time=02-00:00:00
+#SBATCH --nodes=50
 #SBATCH --error=%x_%A.err 
 #SBATCH --output=%x_%A.out
-#SBATCH --nodes=16
 #SBATCH --tasks-per-node=1
+
+#----------- Header --------------
+echo -e "# Job: ${SLURM_JOB_NAME}\tid: ${SLURM_JOB_ID}"
+echo -e "# Total Nodes: ${SLURM_JOB_NUM_NODES}\tTasks per Node: ${SLURM_NTASKS_PER_NODE}\tCPUS in master: ${SLURM_JOB_CPUS_PER_NODE}"
+echo -e "# Nodes List: ${SLURM_JOB_NODELIST}\tQOS: ${SLURM_JOB_QOS}"
+echo -e "# Account: ${SLURM_JOB_ACCOUNT}\tSubmitter host: ${SLURM_SUBMIT_HOST}\tRunning Host: ${SLURMD_NODENAME}"
+echo -e "# Start: $(date)"
+T=$(date +%s)
+echo -n "# " && printf '%.0s-' {1..50} && echo # prints a line
+#------------ Main ----------------
 
 if (($# < 1)); then
     (>&2 echo "Error missing parameters")
     exit
 fi
 
-for i in 1 2 4 8 16; do
-    for j in 1 2 4 8 16; do
-	END=$1
-	times=$((SLURM_JOB_NUM_NODES/i))     # Estimates nproc/step
-	char='%.0s -s '$i                    # generates a substring '%0s -s step'
-	input=$(printf "${char}" $(eval echo {1..$times}))
-	echo "Running times="$times
-	echo $input    
+END=$1
+
+for i in {1..9} {11..39..2} ; do
+    for j in {1..8} {10..40..2} {24..40..4}; do
 	for ((a=1; a<=END; ++a)); do
-	    mpirun -np 1 ./dynamicOOP -s i -s j
+	    mpirun -np 1 ./dynamicOOP -s $i -s $j -i
 	done
     done    
 done
+
+#------------- Foot ----------------
+echo -n "# " && printf '%.0s-' {1..50} && echo # prints a line
+echo -e "# Job: "${SLURM_JOB_NAME}"\tid: "${SLURM_JOB_ID}"\tend: "$(date)
+T=$(($(date +%s)-T))
+printf "# Elapsed time: %02d:%02d:%02d:%02d\n" "$((T/86400))" "$((T/3600%24))" "$((T/60%60))" "$((T%60))"
+echo "# Time in seconds: ${T}"
+echo -n "# " && printf '%.0s-' {1..50} && echo # prints a line

80d93a57e07c1cca2973897bd9cc19ee0fe1f64e
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2021-03-30 07:05:41 +0200

    Add trace.sh

--- utils/trace.sh
+++ utils/trace.sh
@@ -1,11 +1,36 @@
 #!/usr/bin/env bash
 
+# Copyright (C) 2021  Jimmy Aguilar Mena
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 export EXTRAE_ON=1
 export EXTRAE_CONFIG_FILE=extrae.xml
-export LD_PRELOAD=${EXTRAE_HOME}/lib/libnanosmpitrace.so
+
+# Remove previous traces
+if  [ -z ${MPI_LOCALRANKID} ] || [ $MPI_LOCALRANKID -eq 0 ]; then
+	echo "Deleting old traces"
+	rm -rf TRACE.* set-0/ *.prv *.row *.pcf
+fi
+
 export LD_PRELOAD=${EXTRAE_HOME}/lib/libompitrace.so
+$@
+unset LD_PRELOAD
 
-export NANOS6_CONFIG=@PROJECT_BINARY_DIR@/nanos6.toml
-export NANOS6_CONFIG_OVERRIDE="version.debug=false,version.instrument="extrae",cluster.disable_remote=false"
-
-taskset -c 0-8 ./$@
+# Create the mpits
+if  [ -z ${MPI_LOCALRANKID} ] || [ $MPI_LOCALRANKID -eq 0 ]; then
+	echo "Creating mpits file manually"
+	ls ${PWD}/set-0/*.mpit | sed 's/$/ named/' > TRACE.mpits
+fi

93c107cf913ec8e7f36566c35f61473f1e72962b
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2021-04-05 05:05:36 +0200

    Fix CMakeLists installation with configure_file.

--- matmul_matvec_mpi/matmul_omp_mpi.c
+++ matmul_matvec_mpi/matmul_omp_mpi.c
@@ -108,7 +108,6 @@ int main(int argc, char **argv)
 	if (env.rank == 0) {
 		create_reportable_int("worldsize", env.worldsize);
 		create_reportable_int("cpu_count", env.cpu_count);
-		create_reportable_int("maxthreads", env.maxthreads);
 		report_args();
 	}
 

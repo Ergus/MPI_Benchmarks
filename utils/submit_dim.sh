dee862aca05d08e77e26552ce3d5f7fd3275c937
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2022-02-14 03:50:06 +0100

    Update submit scripts.

--- utils/submit_dim.sh
+++ utils/submit_dim.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-#SBATCH --workdir=.
+#SBATCH --signal=B:SIGUSR1@120
 
-#SBATCH --tasks-per-node=2
-#SBATCH --cpus-per-task=24
+function got_signal() {
+	echo "# $(date): Got exit signal"
+}
+
+trap got_signal SIGUSR1
 
 # Declare command line arguments.
 # In this script there are not default values to prevent errors.
@@ -11,8 +14,8 @@ source @PROJECT_BINARY_DIR@/argparse.sh
 
 # Arguments to use in this script
 add_argument -a R -l repeats -h "Repetitions per program" -t int
-add_argument -a C -l cores -h "Number of cores per node" -t int
 add_argument -a N -l ntasks -h "number of tasks" -t int
+add_argument -a C -l cores -h "Number of cores per task" -t int
 
 # Arguments for the executable
 add_argument -a D -l dim -h "Matrix dimension" -t int
@@ -29,13 +32,16 @@ BS=${ARGS[B]}
 REPEATS=${ARGS[R]}
 ITS=${ARGS[I]}
 
-NTASTS=${ARGS[n]}
+NTASKS=${ARGS[N]}
 CORES=${ARGS[C]}
+
+# special nanos variables needed to set.
+export NANOS6_CONFIG=@PROJECT_BINARY_DIR@/nanos6.toml
 
 # Start run here printing run info header
 echo "# Job: ${SLURM_JOB_NAME} id: ${SLURM_JOB_ID}"
 echo "# Nodes: ${SLURM_JOB_NUM_NODES} Cores_per_node: ${SLURM_JOB_CPUS_PER_NODE}"
-echo "# Ntasks: ${NTASTS} Tasks_per_Node: ${SLURM_NTASKS_PER_NODE}"
+echo "# Ntasks: ${NTASKS} Tasks_per_Node: ${SLURM_NTASKS_PER_NODE}"
 echo "# Nodes_List: ${SLURM_JOB_NODELIST}"
 echo "# QOS: ${SLURM_JOB_QOS}"
 echo "# Account: ${SLURM_JOB_ACCOUNT} Submitter_host: ${SLURM_SUBMIT_HOST} Running_Host: ${SLURMD_NODENAME}"
@@ -46,28 +52,45 @@ printargs "# "
 
 # Print nanos6 environment variables
 env | grep NANOS6 | sed -e 's/^#*/# /'
-echo "# ======================================"
+echo ""
 
 if [ $((SLURM_JOB_NUM_NODES*BS<=DIM)) != 1 ]; then
 	echo "# Jump combination nodes: $node, dim: $rows, bs: $BS"
 	exit
 fi
 
-for EXE in @TEST@_*; do
-	COMMAND="srun --ntasks=${NTASTS} taskset -c 0-$((CORES - 1)) ./${EXE} $DIM $BS $ITS"
+EXES=${ARGS[REST]}
+EXES_COUNT=$(echo $EXES | wc -w)
 
-	echo -e "# Starting command: ${COMMAND}"
-	echo "# ======================================"
-	for ((it=0; it<${REPEATS}; ++it)) {
-		echo "# Starting it: ${it} at: $(date)"
-		start=${SECONDS}
-		${COMMAND}
-		end=${SECONDS}
-		echo "# Ending: $(date)"
-		echo "# Elapsed: $((end-start))"
-		echo "# --------------------------------------"
-	}
-	echo ""
+for EXE in ${EXES}; do
+	# Check that EXE exists and is an executable
+	if [[ ! -x $EXE ]]; then
+		echo "# WARN: Input: ${EXE} is not an executable file"
+		continue
+	fi
+
+	echo "# Starting executable: ${EXE} $((++EXECOUNT))/${EXES_COUNT}"
+	for DISABLE_REMOTE in false true; do  # namespace enable/disable
+
+		# Run only once for mpi benchmarks
+		[ $DISABLE_REMOTE == true ] && [ ${EXE##*_} == 'mpi' ] && continue
+
+		export NANOS6_CONFIG_OVERRIDE="cluster.disable_remote=${DISABLE_REMOTE}"
+
+		COMMAND="srun --cpu-bind=cores --ntasks=${NTASKS} --cpus-per-task=${CORES} ./${EXE} $DIM $BS $ITS"
+
+		echo -e "# Starting command: ${COMMAND}"
+		echo "# ======================================"
+		for it in $(seq ${REPEATS}); do
+			echo "# Starting it: ${it} $(date)"
+			start=${SECONDS}
+			${COMMAND}
+			end=${SECONDS}
+			echo "# Ending: $(date)"
+			echo "# Elapsed: $((end-start)) accumulated $((end-init))"
+			echo "# --------------------------------------"
+		done
+	done
 done
 
 # We arrive here only when not wall time was reached.
--- utils/submiter_dim.sh
+++ utils/submiter_dim.sh
@@ -19,41 +19,89 @@ source @PROJECT_BINARY_DIR@/argparse.sh
 
 # Arguments for this script
 add_argument -a w -l wtime -h "Wall time limit for jobs" -t timer -d 12:00:00
-add_argument -a q -l queue -h "Cluster queue" -t enum -e "debug bsc_cs xlarge" -d "bsc_cs"
+add_argument -a q -l queue -h "Cluster queue" -t enum -e debug,bsc_cs,xlarge -d bsc_cs
+add_argument -a o -l output -h "Output directory" -t string -d "results"
 
 # Arguments to bypass
 add_argument -a R -l repeats -h "Program repetitions default[3]" -t int -d 3
-add_argument -a C -l cores -h "Number of cores per node" -t int -d 48
+add_argument -a N -l ntasks -h "Number of tasks" -t list -d 1,2,4,8,16,32
+add_argument -a C -l cores -h "Number of cores per node" -t list -d 12,24,48
+add_argument -a F -l fitnodes -h "Fit processes in nodes" -t bool
 
 add_argument -a D -l dim -h "Matrix dimension" -t int
-add_argument -a B -l BS -h "Blocksize" -t int
+add_argument -a B -l BS -h "Blocksize" -t list
 add_argument -a I -l iterations -h "Program interations default[5]" -t int -d 5
 
 parse_args "$@"
+
+mkdir -p "${ARGS[o]}"
+logfile="${ARGS[o]}/submit.log"
+
+# Redirect all the outputs to the logfile
+exec &> >(tee -a ${ARGS[o]}/submit.log)
+
+echo "# Submit time: $(date)"
+echo "# Args: $*"
 printargs "# "
 
-jobname="@TEST@_${ARGS[D]}_${ARGS[B]}_${ARGS[I]}_${ARGS[C]}"
+# Check that there are executables to run
+if [ -z "${ARGS[REST]}" ]; then
+    echo "Error: No input executable provided ('ARGS[REST]' is empty)" >&2
+    exit 1
+fi
 
-mkdir -p "results/${jobname}"
-echo "# Output directory: ${jobname}"
+for EXE in ${ARGS[REST]}; do
+    if [ ! -x $EXE ]; then    # Check all the files are executable
+        echo "Error: '$EXE' is not an executable file" >&2
+        exit 2
+    fi
 
-ntasks=(1 2 4 8 16 32)
-echo "# List num ntasks: ${nodes[*]}"
-
-for ntask in ${ntasks[@]}; do
-	echo "# Submitting for ${ntask} task[s]"
-
- 	sbatch --ntasks=${ntask} \
-		   --time=${ARGS[w]} \
-		   --qos=${ARGS[q]} \
- 		   --job-name="${jobname}/${ntask}" \
- 		   --output="results/%x_%j.out" \
- 		   --error="results/%x_%j.err" \
- 		   ./submit_@TEST@_dim.sh \
-		   -R ${ARGS[R]} \
-		   -D ${ARGS[D]} \
-		   -B ${ARGS[B]} \
-		   -I ${ARGS[I]} \
-		   -N ${ntask}   \
-		   -C ${ARGS[C]}
+    PREFIX=${EXE%%_*}
+    if [ -z ${TEST} ]; then   # check all inputs have a common prefix.
+        TEST=${PREFIX}
+    elif [ ${TEST} != ${PREFIX} ]; then
+        echo "Error: No common prefix (${TEST} != ${PREFIX})" >&2
+        exit 3
+    fi
 done
+
+# If we are here then we can start the submission.
+for CORES in ${ARGS[C]}; do
+    for BS in ${ARGS[B]}; do
+
+        JOBPREFIX="${TEST}_${ARGS[D]}_${BS}_${ARGS[I]}_${CORES}"
+
+        OUTDIR="${ARGS[o]}/${JOBPREFIX}"
+        mkdir -p ${OUTDIR}
+        echo "# Output directory: ${OUTDIR}"
+
+        for NTASKS in ${ARGS[N]}; do
+            if [ ${ARGS[F]} == true ]; then # One process per node
+                nodes=$(( (NTASKS * CORES + 48 - 1) / 48 ))
+            else
+                nodes=${NTASKS}
+            fi
+            command="sbatch --nodes=${nodes} \
+                            --exclusive \
+                            --time=${ARGS[w]} \
+                            --qos=${ARGS[q]} \
+                            --job-name="${JOBPREFIX}/${NTASKS}" \
+                            --output="${ARGS[o]}/%x_%j.out" \
+                            --error="${ARGS[o]}/%x_%j.err" \
+                            --workdir=. \
+                            ./submit_dim.sh \
+                            -R ${ARGS[R]} \
+                            -D ${ARGS[D]} \
+                            -B ${BS} \
+                            -I ${ARGS[I]} \
+                            -N ${NTASKS}   \
+                            -C ${CORES} \
+                            ${ARGS[REST]} "
+
+            # Print and execute command
+            echo ${command// +/ }
+            ${command}
+        done # NTASKS
+    done # BS
+done # CORES
+echo ""
